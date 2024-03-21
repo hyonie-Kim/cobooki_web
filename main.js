e5f12793f015cae78034e316ed026d559122fe38
hyonie <dev.hyonie@gmail.com>
2024-03-21 19:24:05 +0900

    feat: 도서목록 구현 메인

--- main.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.querySelector('.nav_toogleBtn');
-const menu = document.querySelector('.nav_menu');
-const login = document.querySelector('.nav_login');
-
-toggleBtn.addEventListener('click', () => {
-  //active가 없다면 추가, 있다면 제거
-  menu.classList.toggle('active');
-  login.classList.toggle('active');
-});
